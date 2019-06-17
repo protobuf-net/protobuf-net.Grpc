6ff6841cf0bc056caa58f9d18833f08a392524df
mgravell <marc.gravell@gmail.com>
2019-06-17 13:59:29 +0100

    F# - doesn't compile right now

---
+++ examples/pb-net-grpc/Client_FS/Program.fs
@@ -0,0 +1,14 @@
+﻿open System
+open ProtoBuf.Grpc.Client
+open Shared_FS
+open System.Net.Http
+
+
+[<EntryPoint>]
+let main argv : int =
+    ClientFactory.AllowUnencryptedHttp2 = true |> ignore
+    use http = new HttpClient ( BaseAddress = new Uri("http://localhost:10042") )
+    let client = ClientFactory.Create<ICalculator>(http)
+    let result = client.Multiply(new MultiplyRequest(X = 12, Y = 4)) 
+    printfn "%i" result.Result
+    0 // return an integer exit code
---
+++ examples/pb-net-grpc/Shared_FS/MyContracts.fs
@@ -0,0 +1,22 @@
+﻿namespace Shared_FS
+
+open System.ServiceModel
+open System.Threading.Tasks
+open System.Runtime.Serialization
+
+[<DataContract>]
+type MultiplyRequest =
+    {   [<DataMember(Order = 1)>] mutable X : int;
+        [<DataMember(Order = 2)>] mutable Y : int;
+    }
+    
+[<DataContract>]
+type MultiplyResult =
+    {   [<DataMember(Order = 1)>] mutable Result : int;
+    }
+
+[<ServiceContract(Name = "Hyper.Calculator")>]
+type ICalculator =
+    abstract MultiplyAsync : request : MultiplyRequest -> ValueTask<MultiplyResult>
+    abstract Multiply : request : MultiplyRequest -> MultiplyResult
+
