9e3f51066f617e4b4c99c6ae2c1641d93abbcac9
mgravell <marc.gravell@gmail.com>
2019-06-19 21:12:44 +0100

    add native grpc examples

---
+++ examples/grpc/Client/Program.cs
@@ -0,0 +1,43 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Hyper;
+using MegaCorp;
+using System;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            
+            var channel = new Channel("localhost", 10042, ChannelCredentials.Insecure);
+            try
+            {
+                var calc = new Calculator.CalculatorClient(channel);
+                for (int i = 0; i < 5; i++)
+                {
+                    using var ma = calc.MultiplyAsync(new MultiplyRequest { X = i, Y = i });
+                    var calcResult = await ma.ResponseAsync;
+                    Console.WriteLine(calcResult.Result);
+                }
+
+
+
+                var clock = new TimeService.TimeServiceClient(channel);
+                using var subResult = clock.Subscribe(new Empty());
+                using var reader = subResult.ResponseStream;
+                while (await reader.MoveNext(default))
+                {
+                    var time = reader.Current.Time;
+                    Console.WriteLine($"The time is now {time}");
+                }
+            }
+            finally
+            {
+                await channel.ShutdownAsync();
+            }
+        }
+    }
+}
---
+++ examples/grpc/Server/Program.cs
@@ -0,0 +1,52 @@
+﻿using Grpc.Core;
+using Hyper;
+using System;
+using System.Threading.Tasks;
+using MegaCorp;
+using Google.Protobuf.WellKnownTypes;
+
+namespace MyServer
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            var calc = new MyCalculator();
+            var clock = new MyClock();
+            Server server = new Server
+            {
+                Ports = { new ServerPort("localhost", 10042, ServerCredentials.Insecure) },
+                Services = {
+                    Calculator.BindService(calc),
+                    TimeService.BindService(clock),
+                }
+            };
+            server.Start();
+            Console.WriteLine("Server running... press any key");
+            Console.ReadKey();
+            await server.ShutdownAsync();
+        }
+    }
+
+    class MyCalculator : Calculator.CalculatorBase
+    {
+        public override Task<MultiplyResult> Multiply(MultiplyRequest request, ServerCallContext context)
+        {
+            Console.WriteLine($"Processing request from {context.Peer}");
+            var result = request.X * request.Y;
+            return Task.FromResult(new MultiplyResult { Result = result });
+        }
+    }
+    class MyClock : TimeService.TimeServiceBase
+    {
+        public override async Task Subscribe(Empty request,
+            IServerStreamWriter<TimeResult> responseStream, ServerCallContext context)
+        {
+            for(int i = 0; i < 5; i++)
+            {
+                await responseStream.WriteAsync(new TimeResult { Time = Timestamp.FromDateTime(DateTime.UtcNow) });
+                await Task.Delay(TimeSpan.FromSeconds(5));
+            }
+        }
+    }
+}
