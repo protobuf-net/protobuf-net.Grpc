7743877faa35cf5b1eec09f11d7e5965f9225ba1
mgravell <marc.gravell@gmail.com>
2019-10-10 15:31:42 +0100

    release notes

--- docs/releasenotes.md
+++ docs/releasenotes.md
@@ -1,5 +1,9 @@
 # Release Notes
 
+## 1.0.10
+
+- add alternative API for defining simple services ([via @mythz](https://github.com/protobuf-net/protobuf-net.Grpc/pull/23))
+
 ## 1.0.6
 
 - fix type creation on .NET Standard (thanks @imsh)
--- src/protobuf-net.Grpc/Configuration/IGrpcService.cs
+++ src/protobuf-net.Grpc/Configuration/IGrpcService.cs
@@ -1,6 +1,7 @@
 namespace ProtoBuf.Grpc.Configuration
 {
     /// <summary>
+    /// Marker interface for describing simple gRPC services.
     /// When a type implements this interface, the type name (without any prefix or other qualification)
     /// will be used as the name for resolving the gRPC service. All public methods defined by the type will be mapped as gRPC methods,
     /// using their declared name. 
