b0f3fc378d8d2fbba85b224c4eea5ed91770ff0a
Demis Bellot <demis.bellot@gmail.com>
2019-10-10 10:02:17 -0400

    Update IGrpcService.cs

--- src/protobuf-net.Grpc/Configuration/IGrpcService.cs
+++ src/protobuf-net.Grpc/Configuration/IGrpcService.cs
@@ -1,7 +1,9 @@
 namespace ProtoBuf.Grpc.Configuration
 {
     /// <summary>
-    /// Marker Interface to specify that all public methods on this class should be wired as GRPC Services 
+    /// When a type implements this interface, the type name (without any prefix or other qualification)
+    /// will be used as the name for resolving the gRPC service. All public methods defined by the type will be mapped as gRPC methods,
+    /// using their declared name. 
     /// </summary>
     public interface IGrpcService {}
 }
