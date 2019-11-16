27e074bef5e59c6f1b7dd5e1fd929742699aedda
Demis Bellot <demis.bellot@gmail.com>
2019-11-16 14:27:51 -0500

    Fix `<Protobuf/>` ref include external imports

--- tests/protobuf-net.Grpc.Tool/Program.cs
+++ tests/protobuf-net.Grpc.Tool/Program.cs
@@ -1,11 +1,17 @@
 ﻿using System;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcToolTest;
 
 namespace protobuf_net.Grpc.Tool
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            var client = new GrpcServices.GrpcServicesClient(GrpcChannel.ForAddress("https://example.org"));
+            AllTypes response = await client.GetAllTypesAsync(new AllTypes());
         }
     }
 }
