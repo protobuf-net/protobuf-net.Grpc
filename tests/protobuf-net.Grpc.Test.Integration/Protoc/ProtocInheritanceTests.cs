22f69bd79140433a1f0e8f41da70979769f8a1ec
Demis Bellot <demis.bellot@gmail.com>
2019-12-23 18:14:03 -0500

    Update ProtocInheritanceTests to include basic Protoc inheritence test

--- tests/protobuf-net.Grpc.Test.Integration/Protoc/ProtocInheritanceTests.cs
+++ tests/protobuf-net.Grpc.Test.Integration/Protoc/ProtocInheritanceTests.cs
@@ -11,6 +11,27 @@ namespace ProtobufNet.Grpc.Test.Integration.Protoc
         [Fact]
         public void CanDeserializeInheritedTypesWithProtoc()
         {
+            var from = new Sub {
+                Long = 1,
+                // Cannot populate inherited properties: 
+                // String = "Base", 
+                // StringDictionary = new Dictionary<string, string> {
+                //     {"A", "OK"}
+                // }
+            };
+
+            using var ms = new MemoryStream();
+            from.WriteTo(ms);
+            ms.Position = 0;
+
+            var to = Sub.Parser.ParseFrom(ms);
+
+            Assert.Equal(from.Long, to.Long);
+        }
+        
+        [Fact]
+        public void CanDeserializeProtocInheritedTypesWithProtobufNet()
+        {
             var from = new Sub { 
                 Long = 1,
                 // Cannot populate inherited properties: 
@@ -23,14 +44,14 @@ namespace ProtobufNet.Grpc.Test.Integration.Protoc
             using var ms = new MemoryStream();
             from.WriteTo(ms);
             ms.Position = 0;
-            //throws System.InvalidOperationException : Type is not expected, and no contract can be inferred: ProtobufNet.Grpc.Test.Integration.Protoc.Sub
-            var to = Serializer.Deserialize<Sub>(ms); 
+            //throws System.InvalidOperationException : It was not possible to prepare a serializer for: ProtobufNet.Grpc.Test.Integration.Base
+            var to = Serializer.Deserialize<Integration.Sub>(ms); 
             
             Assert.Equal(from.Long, to.Long);
             // Cannot populate inherited properties: 
             // Assert.Equal(from.String, to.String);
             // Assert.Equal(from.StringDictionary["A"], to.StringDictionary["A"]);
         }
-        
+
     }
 }
