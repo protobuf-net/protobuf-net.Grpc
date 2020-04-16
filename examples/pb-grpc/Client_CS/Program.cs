1ea6cb0f728e7d2baa94a8e3d0463691e8dcd284
Abin <3484074+AAATechGuy@users.noreply.github.com>
2020-04-16 04:57:41 -0700

    add example for Native gRPC setup in .NET framework (#82)
    Co-authored-by: Abin Abraham Alichan <abinaa@microsoft.com>

---
+++ examples/pb-grpc/Client_CS/Program.cs
@@ -0,0 +1,29 @@
+﻿using Grpc.Core;
+using ProtoBuf.Grpc.Client;
+using Shared_CS;
+using System;
+using System.Threading.Tasks;
+
+namespace Client_CS
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            var channel = new Channel("localhost", 10042, ChannelCredentials.Insecure);
+            try
+            {
+                var calculator = channel.CreateGrpcService<ICalculator>();
+                var response = await calculator.MultiplyAsync(new MultiplyRequest() { X = 2, Y = 4 });
+                if (response.Result != 8)
+                {
+                    throw new InvalidOperationException();
+                }
+            }
+            finally
+            {
+                await channel.ShutdownAsync();
+            }
+        }
+    }
+}
---
+++ examples/pb-grpc/Shared_CS/MyContracts.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Threading.Tasks;
+
+namespace Shared_CS
+{
+    [ServiceContract(Name = "Hyper.Calculator")]
+    public interface ICalculator
+    {
+        ValueTask<MultiplyResult> MultiplyAsync(MultiplyRequest request);
+    }
+
+    [DataContract]
+    public class MultiplyRequest
+    {
+        [DataMember(Order = 1)]
+        public int X { get; set; }
+
+        [DataMember(Order = 2)]
+        public int Y { get; set; }
+    }
+
+    [DataContract]
+    public class MultiplyResult
+    {
+        [DataMember(Order = 1)]
+        public int Result { get; set; }
+    }
+}
