681a490056bb62ea730d21fa3d7d012cf59ec64b
Marc Gravell <marc.gravell@gmail.com>
2020-05-17 12:57:08 +0100

    client factories

---
+++ src/protobuf-net.Grpc.ClientFactory/ServicesExtensions.cs
@@ -0,0 +1,43 @@
+﻿using Grpc.Net.ClientFactory;
+using Microsoft.Extensions.DependencyInjection;
+using ProtoBuf.Grpc.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProtoBuf.Grpc.ClientFactory
+{
+    /// <summary>
+    /// Provides extension methods to the IServiceCollection API
+    /// </summary>
+    public static class ServicesExtensions
+    {
+        /// <summary>
+        /// Registers a provider that can recognize and handle code-first services
+        /// </summary>
+        public static IHttpClientBuilder AddCodeFirstGrpcClient<T>(this IServiceCollection services) where T : class
+            => services.AddGrpcClient<T>().ConfigureCodeFirstGrpcClient<T>();
+
+        /// <summary>
+        /// Registers a provider that can recognize and handle code-first services
+        /// </summary>
+        public static IHttpClientBuilder AddCodeFirstGrpcClient<T>(this IServiceCollection services,
+            string name) where T : class
+            => services.AddGrpcClient<T>(name).ConfigureCodeFirstGrpcClient<T>();
+
+        /// <summary>
+        /// Registers a provider that can recognize and handle code-first services
+        /// </summary>
+        public static IHttpClientBuilder AddCodeFirstGrpcClient<T>(this IServiceCollection services,
+            Action<GrpcClientFactoryOptions> configureClient) where T : class
+            => services.AddGrpcClient<T>(configureClient).ConfigureCodeFirstGrpcClient<T>();
+
+        /// <summary>
+        /// Configures the provided client-builder to use code-first GRPC for client creation
+        /// </summary>
+        public static IHttpClientBuilder ConfigureCodeFirstGrpcClient<T>(this IHttpClientBuilder clientBuilder) where T : class
+            => clientBuilder.ConfigureGrpcClientCreator(
+                (services, callInvoker) => Client.GrpcClientFactory.CreateGrpcService<T>(callInvoker,
+                    services.GetService<Configuration.ClientFactory>()));
+    }
+}
---
+++ tests/protobuf-net.Grpc.Test/ClientFactoryTests.cs
@@ -0,0 +1,62 @@
+﻿#if CLIENT_FACTORY
+
+using Grpc.Net.ClientFactory;
+using Microsoft.Extensions.DependencyInjection;
+using ProtoBuf.Grpc.ClientFactory;
+using System;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Threading.Tasks;
+using Xunit;
+namespace protobuf_net.Grpc.Test
+{
+    public class ClientFactoryTests
+    {
+        [Fact]
+        public void CanConfigureCodeFirstClient()
+        {
+            var services = new ServiceCollection();
+            services.AddCodeFirstGrpcClient<IMyService>(o =>
+            {
+                o.Address = new Uri("http://localhost");
+            });
+            var serviceProvider = services.BuildServiceProvider();
+            
+            var clientFactory = serviceProvider.GetRequiredService<GrpcClientFactory>();
+            var client = clientFactory.CreateClient<IMyService>(nameof(IMyService));
+
+            Assert.NotNull(client);
+            var name = client.GetType().FullName;
+            Assert.StartsWith("ProtoBuf.Grpc.Internal.Proxies.ClientBase.", name);
+        }
+
+        [Fact]
+        public void CanConfigureHttpClientBuilder()
+        {
+            var services = new ServiceCollection();
+            services.AddGrpcClient<IMyService>(o =>
+            {
+                o.Address = new Uri("http://localhost");
+            }).ConfigureCodeFirstGrpcClient<IMyService>();
+            var serviceProvider = services.BuildServiceProvider();
+
+            var clientFactory = serviceProvider.GetRequiredService<GrpcClientFactory>();
+            var client = clientFactory.CreateClient<IMyService>(nameof(IMyService));
+
+            Assert.NotNull(client);
+            var name = client.GetType().FullName;
+            Assert.StartsWith("ProtoBuf.Grpc.Internal.Proxies.ClientBase.", name);
+        }
+    }
+
+    [ServiceContract]
+    public interface IMyService
+    {
+        [OperationContract]
+        public ValueTask<Dummy> UnaryCall(Dummy value);
+    }
+
+    [DataContract]
+    public class Dummy { }
+}
+#endif
