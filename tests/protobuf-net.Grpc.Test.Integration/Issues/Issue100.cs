bb1961be203bfe9c568721a4e63d604487972d5c
Marc Gravell <marc.gravell@gmail.com>
2020-06-23 22:14:29 +0100

    Fix issue with sub-type length mismatch (#102)
    * add failing tests for #100
    
    * consume pb-net 3.0.1; fix #100

---
+++ tests/protobuf-net.Grpc.Test.Integration/Issues/Issue100.cs
@@ -0,0 +1,162 @@
+﻿using Grpc.Core;
+using ProtoBuf;
+using ProtoBuf.Grpc.Configuration;
+using System;
+using System.Threading.Tasks;
+using ProtoBuf.Grpc.Server;
+using Xunit;
+using ProtoBuf.Grpc.Client;
+using Xunit.Abstractions;
+using ProtoBuf.Meta;
+using System.Buffers;
+using Xunit.Sdk;
+using System.IO;
+using System.Runtime.InteropServices;
+
+#nullable disable
+
+namespace protobuf_net.Grpc.Test.Integration.Issues
+{
+    public class Issue100 : IClassFixture<Issue100.Issue100ServerFixture>
+    {
+#if !NET472 // need ABW
+        [Fact]
+        public async Task MeasuredSerialize()
+        {
+            var obj = await _server.GetTestInstance();
+            var ms = new MemoryStream();
+            Serializer.Serialize(ms, obj); // regular serialize
+            Assert.Equal(14, ms.Length);
+            var expected = BitConverter.ToString(ms.GetBuffer(), 0, (int)ms.Length);
+            Log(expected);
+            Assert.Equal("0A-0C-0A-05-0A-03-64-65-66-12-03-61-62-63", expected);
+
+            /*
+Field #1: 0A String Length = 12, Hex = 0C, UTF8 = "  defabc"
+As sub-object :
+  Field #1: 0A String Length = 5, Hex = 0C, UTF8 = " def"
+  As sub-object :
+    Field #1: 0A String Length = 3, Hex = 0C, UTF8 = "def"
+  Field #2: 65 String Length = 3, Hex = 66, UTF8 = "abc"
+             */
+
+
+            // now try measured
+            if ((object)RuntimeTypeModel.Default is IMeasuredProtoOutput<IBufferWriter<byte>> writer)
+            {
+                using var measured = writer.Measure(obj);
+                Assert.Equal(ms.Length, measured.Length);
+
+                var abw = new ArrayBufferWriter<byte>();
+                writer.Serialize(measured, abw);
+                Assert.Equal(ms.Length, abw.WrittenCount);
+
+                var mem = abw.WrittenMemory;
+                Assert.True(MemoryMarshal.TryGetArray(mem, out var segment));
+                var actual = BitConverter.ToString(segment.Array, segment.Offset, segment.Count);
+                Log(actual);
+                Assert.Equal(expected, actual);
+            }
+        }
+#endif
+
+        const int Port = 12315;
+
+        [Service]
+        public interface ITest
+        {
+            ValueTask<TestObject> GetTestInstance();
+        }
+
+        
+        private readonly ITestOutputHelper _log;
+        ITest _server;
+        private void Log(string message) => _log?.WriteLine(message);
+
+
+        public Issue100(Issue100ServerFixture server, ITestOutputHelper log)
+        {
+            _server = server;
+            _log = log;
+            GrpcClientFactory.AllowUnencryptedHttp2 = true;
+        }
+
+        public class Issue100ServerFixture : ITest, IAsyncDisposable
+        {
+            public ValueTask DisposeAsync() => new ValueTask(_server.KillAsync());
+
+            private readonly Server? _server;
+            public Issue100ServerFixture()
+            {
+                _server = new Server
+                {
+                    Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+                };
+                _server.Services.AddCodeFirst(this);
+                _server.Start();
+            }
+            ValueTask<TestObject> ITest.GetTestInstance()
+            {
+                var obj = new TestObject
+                {
+                    Test = new TestThingy { SomeText = "abc", SomeText2 = "def" }
+                };
+                return new ValueTask<TestObject>(obj);
+            }
+        }
+
+        [Fact]
+        public async Task Issue100_UnmanagedClient()
+        {
+            var channel = new Channel("localhost", Port, ChannelCredentials.Insecure);
+            try
+            {
+                var client = channel.CreateGrpcService<ITest>();
+                var obj = await client.GetTestInstance();
+                Assert.Equal("abc", obj.Test.SomeText);
+                Assert.Equal("def", obj.Test.SomeText2);
+            }
+            finally
+            {
+                await channel.ShutdownAsync();
+            }
+        }
+
+#if NETCOREAPP3_1
+        [Fact]
+        public async Task Issue100_ManagedClient()
+        {
+            using var http = global::Grpc.Net.Client.GrpcChannel.ForAddress($"http://localhost:{Port}");
+            var client = http.CreateGrpcService<ITest>();
+            var obj = await client.GetTestInstance();
+            Assert.Equal("abc", obj.Test.SomeText);
+            Assert.Equal("def", obj.Test.SomeText2);
+        }
+#endif
+
+
+        [ProtoContract]
+        public class TestObject
+        {
+            [ProtoMember(1)]
+            public TestThingy Test { get; set; }
+        }
+
+        [ProtoContract]
+        [ProtoInclude(1, typeof(TestThingy))]
+        public abstract class TestBase
+        {
+            [ProtoMember(2)]
+            public string SomeText { get; set; }
+
+            public abstract string SomeText2 { get; set; }
+        }
+
+        [ProtoContract]
+        public class TestThingy : TestBase
+        {
+            [ProtoMember(1)]
+            public override string SomeText2 { get; set; }
+        }
+    }
+}
