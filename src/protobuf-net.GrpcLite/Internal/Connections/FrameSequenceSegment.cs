81707805f16b41570395d161d33f510f3eb14a56
Marc Gravell <marc.gravell@gmail.com>
2022-03-23 15:45:43 +0000

    nit namespaces

--- src/protobuf-net.GrpcLite/Internal/Connections/FrameSequenceSegment.cs
+++ src/protobuf-net.GrpcLite/Internal/Connections/FrameSequenceSegment.cs
@@ -1,54 +1,53 @@
 ﻿using ProtoBuf.Grpc.Lite.Connections;
 using System.Buffers;
 
-namespace ProtoBuf.Grpc.Lite.Internal.Connections
+namespace ProtoBuf.Grpc.Lite.Internal.Connections;
+
+internal sealed class FrameSequenceSegment : ReadOnlySequenceSegment<byte>
 {
-    internal sealed class FrameSequenceSegment : ReadOnlySequenceSegment<byte>
+    public static ReadOnlySequence<byte> Create(in ReadOnlySpan<Frame> frames)
     {
-        public static ReadOnlySequence<byte> Create(in ReadOnlySpan<Frame> frames)
+        switch (frames.Length)
         {
-            switch (frames.Length)
-            {
-                case 0:
-                    return default;
-                case 1:
-                    var buffer = frames[0].GetPayload();
-                    return buffer.IsEmpty ? default : new ReadOnlySequence<byte>(buffer);
-            }
-
-            var iter = frames.GetEnumerator();
-            iter.MoveNext();
-            FrameSequenceSegment? first = null, last = null;
-            foreach (var frame in frames)
-            {
-                var payload = frame.GetPayload(); // don't bother checking length; we don't *expect* any empties
-                if (!payload.IsEmpty)
-                {
-                    // since there are headers between payloads, we are never
-                    // able to merge frames to make a larger payload buffer; that's fine
-                    last = new FrameSequenceSegment(last, payload);
-                    if (first is null) first = last;
-                }
-            }
-
-            if (first is null) return default;
-            if (ReferenceEquals(first, last)) return new ReadOnlySequence<byte>(first.Memory);
-            return new ReadOnlySequence<byte>(first, 0, last!, last!.Memory.Length);
+            case 0:
+                return default;
+            case 1:
+                var buffer = frames[0].GetPayload();
+                return buffer.IsEmpty ? default : new ReadOnlySequence<byte>(buffer);
         }
 
-        private FrameSequenceSegment(FrameSequenceSegment? previous, ReadOnlyMemory<byte> memory) : base()
+        var iter = frames.GetEnumerator();
+        iter.MoveNext();
+        FrameSequenceSegment? first = null, last = null;
+        foreach (var frame in frames)
         {
-            Memory = memory;
-            Next = null;
-            if (previous is not null)
+            var payload = frame.GetPayload(); // don't bother checking length; we don't *expect* any empties
+            if (!payload.IsEmpty)
             {
-                previous.Next = this;
-                RunningIndex = previous.RunningIndex + previous.Memory.Length;
+                // since there are headers between payloads, we are never
+                // able to merge frames to make a larger payload buffer; that's fine
+                last = new FrameSequenceSegment(last, payload);
+                if (first is null) first = last;
             }
-            else
-            {
-                RunningIndex = 0;
-            }
+        }
+
+        if (first is null) return default;
+        if (ReferenceEquals(first, last)) return new ReadOnlySequence<byte>(first.Memory);
+        return new ReadOnlySequence<byte>(first, 0, last!, last!.Memory.Length);
+    }
+
+    private FrameSequenceSegment(FrameSequenceSegment? previous, ReadOnlyMemory<byte> memory) : base()
+    {
+        Memory = memory;
+        Next = null;
+        if (previous is not null)
+        {
+            previous.Next = this;
+            RunningIndex = previous.RunningIndex + previous.Memory.Length;
+        }
+        else
+        {
+            RunningIndex = 0;
         }
     }
 }
--- src/protobuf-net.GrpcLite/Internal/Server/LiteConnection.cs
+++ src/protobuf-net.GrpcLite/Internal/Server/LiteConnection.cs
@@ -4,69 +4,68 @@ using System.Collections.Concurrent;
 using System.Diagnostics.CodeAnalysis;
 using System.Threading.Channels;
 
-namespace ProtoBuf.Grpc.Lite.Internal.Server
+namespace ProtoBuf.Grpc.Lite.Internal.Server;
+
+internal sealed class LiteConnection : IWorker, IConnection
 {
-    internal sealed class LiteConnection : IWorker, IConnection
+    private readonly LiteServer _server;
+    private readonly IFrameConnection _connection;
+    private readonly ILogger? _logger;
+
+    private readonly ConcurrentDictionary<ushort, IStream> _streams = new ConcurrentDictionary<ushort, IStream>();
+
+    public int Id { get; }
+
+    void IConnection.Remove(ushort id) => _streams.Remove(id, out _);
+
+    CancellationToken IConnection.Shutdown => _server.ServerShutdown;
+
+    void IConnection.Close(Exception? fault)
     {
-        private readonly LiteServer _server;
-        private readonly IFrameConnection _connection;
-        private readonly ILogger? _logger;
+        // TODO: need a per-stream shutdown source, rather than per-server?
+    }
 
-        private readonly ConcurrentDictionary<ushort, IStream> _streams = new ConcurrentDictionary<ushort, IStream>();
+    // will be null if not started
+    public Task? Complete { get; private set; }
 
-        public int Id { get; }
+    bool IConnection.IsClient => false;
 
-        void IConnection.Remove(ushort id) => _streams.Remove(id, out _);
+    IAsyncEnumerable<Frame> IConnection.Input => _connection;
 
-        CancellationToken IConnection.Shutdown => _server.ServerShutdown;
+    ConcurrentDictionary<ushort, IStream> IConnection.Streams => _streams;
 
-        void IConnection.Close(Exception? fault)
+    public LiteConnection(LiteServer server, IFrameConnection connection, ILogger? logger)
+    {
+        _connection = connection;
+        _logger = logger ?? server.Logger;
+        Id = server.NextStreamId();
+        _server = server;
+        _ = connection.StartWriterAsync(this, out _output, _server.ServerShutdown);
+        _logger.Debug(Id, static (state, _) => $"connection {state} initialized");
+    }
+
+    private readonly ChannelWriter<Frame> _output;
+    ChannelWriter<Frame> IConnection.Output => _output;
+
+    public void Execute()
+    {
+        _logger.SetSource(LogKind.Server, "connection " + Id);
+        _logger.Debug("starting connection executor");
+        Complete ??= this.RunAsync(_logger, _server.ServerShutdown);
+    }
+
+    bool IConnection.TryCreateStream(in Frame initialize, [MaybeNullWhen(false)] out IStream stream)
+    {
+        if (_server.TryCreateStream(initialize.GetPayloadString(), out var serverStream) && serverStream is not null)
         {
-            // TODO: need a per-stream shutdown source, rather than per-server?
+            serverStream.Initialize(initialize.GetHeader().StreamId, _output, _logger, this);
+            stream = serverStream;
+            return true;
         }
-
-        // will be null if not started
-        public Task? Complete { get; private set; }
-
-        bool IConnection.IsClient => false;
-
-        IAsyncEnumerable<Frame> IConnection.Input => _connection;
-
-        ConcurrentDictionary<ushort, IStream> IConnection.Streams => _streams;
-
-        public LiteConnection(LiteServer server, IFrameConnection connection, ILogger? logger)
+        else
         {
-            _connection = connection;
-            _logger = logger ?? server.Logger;
-            Id = server.NextStreamId();
-            _server = server;
-            _ = connection.StartWriterAsync(this, out _output, _server.ServerShutdown);
-            _logger.Debug(Id, static (state, _) => $"connection {state} initialized");
-        }
-
-        private readonly ChannelWriter<Frame> _output;
-        ChannelWriter<Frame> IConnection.Output => _output;
-
-        public void Execute()
-        {
-            _logger.SetSource(LogKind.Server, "connection " + Id);
-            _logger.Debug("starting connection executor");
-            Complete ??= this.RunAsync(_logger, _server.ServerShutdown);
-        }
-
-        bool IConnection.TryCreateStream(in Frame initialize, [MaybeNullWhen(false)] out IStream stream)
-        {
-            if (_server.TryCreateStream(initialize.GetPayloadString(), out var serverStream) && serverStream is not null)
-            {
-                serverStream.Initialize(initialize.GetHeader().StreamId, _output, _logger, this);
-                stream = serverStream;
-                return true;
-            }
-            else
-            {
-                stream = null;
-                return false;
-            }
+            stream = null;
+            return false;
         }
     }
 }
